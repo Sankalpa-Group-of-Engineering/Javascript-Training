5772921a94e89c268875a7ab82f410280df0a411
rahulshah63 <rahul.shah9863@gmail.com>
2025-12-31 16:48:17 +0545

    Day 1 Notes & exercise for Sankalpa Day 1 JS training

---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,17 @@
+// Declaring different variables of different data types
+let firstName = 'Ram' // first name of a person
+let lastName = 'Khadka' // last name of a person
+let country = 'Nepal' // country
+let city = 'Kathmandu' // capital city
+let age = 25 // age in years
+let isMarried = true
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Ram', //name of a person
+  job = 'teacher',
+  live = 'Nepal'
